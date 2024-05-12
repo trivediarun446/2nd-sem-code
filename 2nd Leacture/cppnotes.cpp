967e29337fffab773ba89ca8b1256694e897e85e
Arun Trivedi <155215034+trivediarun446@users.noreply.github.com>
2024-05-12 10:49:23 +0530

    Add files via upload

---
+++ 2nd Leacture/cppnotes.cpp
@@ -0,0 +1,21 @@
+// There are some math function that help as to solve question directly 
+/* for using maths function firstly we need to add a header file name <cmath>
+
+some functions are :- 
+1) sqrt() for square root of any number 
+2) min() for finding the minimum of two number 
+3) max() for finding the max number from two number 
+4) round() for finding the round of two number 
+5) log() for finding the log of any number 
+*/
+
+
+
+//  for finding the lenght of any string we can use lenght()or size() function for counting the lenght of the string
+// syntax      nameoffile.lenght()
+
+
+/*  Escape character	Result	          Description
+   \'	                  '	              Single quote
+   \"                    "	              Double quote
+   \\	                  \	              Backslash    */
